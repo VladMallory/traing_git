3d2edc3393da681582575c4532e9bf2958c4da37
Коваленко Владислав Анатольевич <vladmallory0321@gmail.com>
2025-06-09 14:07:42 +1000

    Commit

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
-package test2
+package main
 
 import "fmt"
 
+func funcAdd(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println("hello world")
-	//
+	//da
+	fmt.Println(funcAdd(1, 2))
 }
