833129553a3f7df439c490e81b667ebe6ed97054
Коваленко Владислав Анатольевич <vladmallory0321@gmail.com>
2025-06-09 14:11:46 +1000

    Commit

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ func funcAdd(x int, y int) int {
 	return x + y
 }
 
+func sliceFunc(x []int) []int {
+	return x
+
+}
+
 func main() {
 	fmt.Println("hello world")
 	//da
 	fmt.Println(funcAdd(1, 2))
+	fmt.Println("Слайс:", sliceFunc([]int{1, 2, 1, 2}))
 }
